9605a983e88c8611a6c738cfe07d38344a1b8c93
Tom Henighan <henighan.2@gmail.com>
2021-07-10 17:31:48 -0700

    some acrobatics to apply required permissions

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "sitedownnotifier_lambda_role" {
-  name = "sitedownnotifier_lambda_role"
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
 
   assume_role_policy = <<EOF
 {
@@ -18,10 +18,33 @@ resource "aws_iam_role" "sitedownnotifier_lambda_role" {
 EOF
 }
 
+resource "aws_iam_policy" "send_sms_policy" {
+  name        = "send_sms_policy"
+  path        = "/"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "sns:Publish",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_send_sms" {
+    role = "${aws_iam_role.lambda_role.name}"
+    policy_arn = aws_iam_policy.send_sms_policy.arn
+}
+
 resource "aws_lambda_function" "sitedownnotifier_lambda" {
   filename      = "lambda_code.zip"
   function_name = "sitedownnotifier"
-  role          = aws_iam_role.sitedownnotifier_lambda_role.arn
+  role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_script.handler"
   source_code_hash = filebase64sha256("lambda_code.zip")
 
